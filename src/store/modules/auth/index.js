b30df3e65250afa8337d2874af9603e4f3fb80ff
Amine Cheraitia <amine-cheraitia@hotmail.com>
2022-02-12 19:03:35 +0100

    --

--- src/store/modules/auth/index.js
+++ src/store/modules/auth/index.js
@@ -23,7 +23,7 @@ export default {
     },
     async login(context, payload) {
       const response = await fetch(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCxJ333Sm5mSEJm2VbmZ8EQ7QOugundan8',
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=-----------------------------',
         {
           method: 'POST',
           body: JSON.stringify({
