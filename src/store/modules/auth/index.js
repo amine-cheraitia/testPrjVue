398412ea5574bd7423ddac039ed87652e157fc7d
amine-cheraitia <39153775+amine-cheraitia@users.noreply.github.com>
2022-02-12 15:24:12 +0100

    Update index.js

--- src/store/modules/auth/index.js
+++ src/store/modules/auth/index.js
@@ -17,7 +17,7 @@ export default {
     /* login() {}, */
     async signup(context, payload) {
       const response = await fetch(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCxJ333Sm5mSEJm2VbmZ8EQ7QOugundan8',
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=-----------------------------',
         {
           method: 'POST',
           body: JSON.stringify({
